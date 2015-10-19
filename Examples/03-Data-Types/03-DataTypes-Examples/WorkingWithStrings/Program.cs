17b5e168cdec4d3241b6b05bde0cf2d517b0485e
Babalev <ivaylo.babalev@gmail.com>
2015-10-19 18:28:41 +0300

    Data types examples and lecture

---
+++ Examples/03-Data-Types/03-DataTypes-Examples/WorkingWithStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string b = "Lazar Sestrimski";
+            string result = "Hello " + b + "!";
+            Console.WriteLine(result); 
+        }
+    }
+}

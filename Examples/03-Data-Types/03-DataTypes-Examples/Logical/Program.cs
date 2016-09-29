fb5d6f7ac52b6ab49a675405a05276ce6024a40e
babalev <ivaylo.babalev@gmail.com>
2016-09-29 19:07:24 +0300

    Data Types

---
+++ Examples/03-Data-Types/03-DataTypes-Examples/Logical/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 5;
+            //int y = 6;
+
+            ////Console.WriteLine(x <= y);
+
+            //bool a = x > y;
+            //Console.WriteLine(!a);
+            //bool b = true;
+            //Console.WriteLine(!b);
+
+            bool x = true;
+            bool y = false;
+
+            Console.WriteLine(x && y); //false
+            Console.WriteLine(x || y); // true
+            Console.WriteLine(x ^ y); // true
+            Console.WriteLine(!x); //false
+            Console.WriteLine(!y); //true
+
+        }
+    }
+}
---
+++ Examples/03-Data-Types/03-DataTypes-Examples/Variables/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ТИП   Име   стойност
+            //int testInt = 4;
+            //Console.WriteLine(testInt);
+
+            //int secondInt = 6;
+
+            //Console.WriteLine(testInt + secondInt);
+
+            //short a = -256;
+            //ushort b = 3;
+
+            //Console.WriteLine(a);
+
+            //bte positiveByte = 200;
+
+            //sbyte negativeByte = -20;
+            //Console.WriteLine(testInt);
+
+            //int lazar = 5 + testInt;
+            //Console.WriteLine(lazar);
+
+            //testInt = 30;
+            //Console.WriteLine(testInt);
+
+            //
+            //double floatingPointNumber = 4.5;
+            //Console.WriteLine(floatingPointNumber);
+
+            //float floatingPoint = 5.6f;
+            //Console.WriteLine(floatingPoint);
+
+            //decimal decimalValue = 6.8m;
+
+            //double doublePI = (double)22 / (double)7;
+            //float floatPI = (float)22 / (float)7;
+            //decimal decimalPI = (decimal)22 / (decimal)7;
+
+            //Console.WriteLine(doublePI);
+            //Console.WriteLine(floatPI);
+            //Console.WriteLine(decimalPI);
+
+            bool isTrue = true;
+            //Console.WriteLine(isTrue);
+
+            char oneSymbol = '\u1234';
+            Console.WriteLine(oneSymbol);
+        }
+    }
+}

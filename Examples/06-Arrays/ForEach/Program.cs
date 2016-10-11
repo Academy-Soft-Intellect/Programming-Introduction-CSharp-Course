6cb74c7cc2bd272bde8ddcafd8480ee499cd87a9
babalev <ivaylo.babalev@gmail.com>
2016-10-11 18:45:31 +0300

    Arrays examples

---
+++ Examples/06-Arrays/ForEach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = new string[] { "We", "Are", "The", "Champions" };
+
+            foreach (var item in stringArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

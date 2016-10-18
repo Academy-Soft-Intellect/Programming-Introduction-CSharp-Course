93f8b5214fd411c7900140bdef4f5f5350819aee
babalev <ivaylo.babalev@gmail.com>
2016-10-18 18:38:52 +0300

    Exam preperation

---
+++ Examples/Preperation/Task03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+            int[] array = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int currentNumber = array[i];
+                if (i % 2 == 0)
+                {
+                    if (currentNumber > maxNumber)
+                    {
+                        maxNumber = currentNumber;
+                    }
+                }
+                else
+                {
+                    if (currentNumber < minNumber)
+                    {
+                        minNumber = currentNumber;
+                    }
+                }
+            }
+
+            Console.WriteLine("Max number is:" + maxNumber);
+            Console.WriteLine("Min number is:" + minNumber);
+        }
+    }
+}
